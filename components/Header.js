255d8040448a93e464d166653e340f6edcf19f81
evan <malcomchege0582@gmail.com>
2023-12-11 15:20:12 +0300

    top header

--- components/Header.js
+++ components/Header.js
@@ -4,9 +4,20 @@ import React from 'react'
 function Header() {
   return (
     <div>
+        
         <Image
-        src=''
+        src='https://ke.jumia.is/cms/2023/STB/KE_FreeDelivery_1122_STB.gif'
+        objectFit='contain'
+        width={1800}
+        height={40}
         />
+        {/* topbar */}
+        <div>
+            <div>
+                
+            </div>
+            <div></div>
+        </div>
     </div>
   )
 }
--- next.config.js
+++ next.config.js
@@ -3,4 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+// next.config.js
+
+module.exports = {
+  images: {
+    domains: ['ke.jumia.is'],
+  },
+}
+
