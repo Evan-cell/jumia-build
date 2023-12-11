bcc79c84a446d79be8ebeff4e35864a32f480c6c
evan <malcomchege0582@gmail.com>
2023-12-11 15:00:08 +0300

    HEADER

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <Image
+        src=''
+        />
+    </div>
+  )
+}
+
+export default Header
