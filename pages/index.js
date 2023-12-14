13d0ec5c12fb71558e54a7643cb6605d1b502ceb
evan <malcomchege0582@gmail.com>
2023-12-14 11:49:04 +0300

    categories

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Header from '@/components/Header'
 import Banner from '@/components/Banner'
+import Categories from '@/components/Categories'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +15,7 @@ export default function Home() {
       <div className='main'>
       <main className='max-w-screen-2xl mx-auto'>
         <Banner/>
+        <Categories className=''/>
       </main>
       </div>
 
